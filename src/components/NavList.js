1b9f47ecb7d9470cb53491837eab7db4ec420658
TommysG <thomasgianx@gmail.com>
2020-03-10 03:28:41 +0200

    merge fix

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const NavList = () => {
-<<<<<<< HEAD
   return (
     <div className="breadcrumbContainer">
       <ul className="breadcrumb">
@@ -15,21 +14,6 @@ const NavList = () => {
       </ul>
     </div>
   );
-=======
-	return (
-		<div className="breadcrumbContainer">
-			<ul className="breadcrumb">
-				<li>
-					<a href="/home">Home</a>
-				</li>
-				<li>
-					<a href="/home">Category</a>
-				</li>
-				<li>Thread</li>
-			</ul>
-		</div>
-	);
->>>>>>> 8b1a7d2fba4050db26cc4e2859304b874113bf59
 };
 
 export default NavList;
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "../css/userInfo.css";
 
 export class UserInfo extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div className="userInfo">
@@ -21,26 +20,6 @@ export class UserInfo extends Component {
       </div>
     );
   }
-=======
-	render() {
-		return (
-			<div className="userInfo">
-				<div className="userProfilePicture">
-					<img
-						src="http://www.azyrusthemes.com/forum2/fonts/icons/avatars/J.svg"
-						alt="Avatar"
-						style={{ position: "relative", width: "100%", height: "100%" }}
-					/>
-					<h4 style={{ width: "100%", textAlign: "center" }}>Jake</h4>
-					<div className="userStats">
-						<h6>Likes: 50</h6>
-						<h6>Posts: 241</h6>
-					</div>
-				</div>
-			</div>
-		);
-	}
->>>>>>> 8b1a7d2fba4050db26cc4e2859304b874113bf59
 }
 
 export default UserInfo;
