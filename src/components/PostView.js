f5732cbe372933ad5aaa4425f613e882eb2b4a5c
LambrosKolovos <kolovosl@csd.auth.gr>
2020-03-11 02:38:53 +0200

    Design update

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -3,29 +3,32 @@ import "../css/postView.css";
 import UserInfo from "../components/UserInfo";
 
 const PostView = ({ title, date, content }) => {
-  return (
-    <div className="postContainer">
-      <UserInfo />
-      <div className="postInfo">
-        <div className="postHeader">
-          <div className="postTitle">
-            <h3>{title}</h3>
-          </div>
-          <div className="postDate">
-            <h5>{date}</h5>
-          </div>
-        </div>
-        <div className="postBody">
-          <p>{content}</p>
-          <div className="likebar">
-            <p>
-              (1)
-              <a href="#heart"> 🤍</a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="postContainer">
+			<UserInfo />
+			<div className="postInfo">
+				<div className="postHeader">
+					<div className="postTitle">
+						<h3>{title}</h3>
+					</div>
+					<div className="postDate">
+						<h5>{date}</h5>
+					</div>
+				</div>
+				<div className="postBody">
+					<p>{content}</p>
+					<div className="likebar">
+						<p>
+							(1)
+							<a href="#heart">
+								{" "}
+								<i class="fa fa-heart"></i>
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default PostView;
