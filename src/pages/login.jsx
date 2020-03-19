d0fa45acdc73d7b590b53a81217a10fd28edac21
lepaleo <palaiocho@csd.auth.gr>
2020-03-19 22:00:52 +0200

    commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,21 +5,24 @@ import "../css/login.css";
 export class login extends Component {
   render() {
     return (
-      <Container className="box-container">
-        <div className="login-container">
+      <Container className="split">
+        <div class="left">
           <form className="login-form" method="post">
-            <img
-              className="background2"
-              src="images/user_icon.png"
-              alt="logo"
-            ></img>
-            <h1>Login</h1>
-            <input
-              className="input-email"
-              type="text"
-              name="email-user"
-              placeholder="Email or Username"
-            />
+            <div class="title">
+              <h1>Healthify</h1>
+              <h1>Login</h1>
+            </div>
+
+            <div class="login-box">
+              <i class="fa fa-user icon"></i>
+              <input
+                className="input-email"
+                type="text"
+                name="email-user"
+                placeholder="Email or Username"
+              />
+            </div>
+
             <input
               className="input-pass"
               type="password"
@@ -30,6 +33,7 @@ export class login extends Component {
             <a type="submit" href="/home">
               Sumbit
             </a>
+
             <div className="forgot-pass">
               <a type="forgot" name="" href="/home">
                 Forgot Username / Password?
@@ -40,6 +44,15 @@ export class login extends Component {
               <a type="txt2" href="/home">
                 Create your Account
               </a>
+            </div>
+          </form>
+        </div>
+
+        <div class="split right">
+          <form className="signup-form" method="post">
+            <div class="title2">
+              <h1>Healthify</h1>
+              <h1>Sign Up</h1>
             </div>
           </form>
         </div>
