d21616315ca625986117faf7ee641b1042c90478
LambrosKolovos <kolovosl@csd.auth.gr>
2020-04-26 22:44:57 +0300

    searchbar redesign

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "../css/search.css";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import DropdownItem from "react-bootstrap/DropdownItem";
 
 const Search = () => {
-  return (
-    <div className="search-box">
-      <form>
-        <button type="button" className="search-button">
-          <i className="fa fa-search search-icon"></i>
-        </button>
-        <input className="search" type="text" placeholder="Search Topics" />
-      </form>
-    </div>
-  );
+	return (
+		<div className="filter-search">
+			<DropdownButton className="filter-button" title={"Posts"}>
+				<DropdownItem eventKey="1">Articles</DropdownItem>
+			</DropdownButton>
+			<input className="search-field" placeholder="Searching"></input>
+			<button type="button" className="search-button">
+				<i className="fa fa-search search-icon"></i>
+			</button>
+		</div>
+	);
 };
 
 export default Search;
