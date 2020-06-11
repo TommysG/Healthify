edf77daddbbedd0edc96fa075992d680696cfdb8
LambrosKolovos <kolovosl@csd.auth.gr>
2020-06-11 23:45:47 +0300

    login_implementation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "../css/input.css";
 
-const Input = ({ type, label }) => {
-  return (
-    <div className="input-field">
-      <input name="input" type={type} autoComplete="off" required />
-      <label htmlFor="input" className="label">
-        <span className="label-name">{label}</span>
-      </label>
-    </div>
-  );
+const Input = ({ type, label, name, fnc }) => {
+	return (
+		<div className="input-field">
+			<input
+				name={name}
+				type={type}
+				autoComplete="off"
+				onChange={fnc}
+				required
+			/>
+			<label htmlFor={name} className="label">
+				<span className="label-name">{label}</span>
+			</label>
+		</div>
+	);
 };
 
 export default Input;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,83 +8,114 @@ import { Row } from "react-bootstrap";
 import Navbar from "../components/Navbar";
 import Backdrop from "../components/Backdrop/Backdrop";
 import SideDrawer from "../components/SideDrawer/SideDrawer";
+import auth from "../Auth.js";
 
 export class login extends Component {
-  state = {
-    sideDrawerOpen: false,
-  };
+	state = {
+		userEmail: "",
+		userPassword: "",
+		sideDrawerOpen: false,
+	};
 
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
-    });
-  };
+	logIn = (event) => {
+		console.log("Checking for: " + this.state.userEmail);
+		console.log("Given password: " + this.state.userPassword);
+		auth.login(this.state.userEmail, this.state.userPassword);
+	};
 
-  backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
-  };
-  render() {
-    let backdrop;
+	getInputText = (event) => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />;
-    }
-    return (
-      <div className="login-page">
-        <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
-        <SideDrawer show={this.state.sideDrawerOpen} />
-        {backdrop}
-        <Container fluid className="login-container">
-          <header className="top-head"></header>
-          <div className="form">
-            <h1 className="login-title">Login</h1>
-            <p className="signup-link">
-              Don't have a Healthify account? <Link to="/signup">Sign Up</Link>
-            </p>
-            <Row className="login-section">
-              <Col xs className="login-email">
-                <Input type="text" label="Email"></Input>
-                <Input type="password" label="Password"></Input>
-                <Link to="/home">
-                  <button className="login-btn" href="/home">
-                    Login
-                  </button>
-                </Link>
-                <div className="sm-btns">
-                  <a className="btn-floating btn-fb" href="#index">
-                    <i className="fab fa-facebook-f"> </i>
-                  </a>
-                  <a className="btn-floating btn-gplus" href="#index">
-                    <i className="fab fa-google-plus-g"> </i>
-                  </a>
-                </div>
-              </Col>
-              <div className="login-social">
-                <div className="btn-container">
-                  <button className="social-btn btn-facebook">
-                    <img
-                      src="https://static.parastorage.com/services/login-statics/1.742.0/images/facebook-logo.svg"
-                      alt="img"
-                    ></img>
-                    <span>Login with Facebook</span>
-                  </button>
-                </div>
-                <div className="btn-container">
-                  <button className="social-btn btn-google">
-                    <img
-                      alt="img"
-                      src="https://static.parastorage.com/services/login-statics/1.742.0/images/google-logo.svg"
-                    ></img>
-                    <span>Login with Google</span>
-                  </button>
-                </div>
-              </div>
-            </Row>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+	drawerToggleClickHandler = () => {
+		this.setState((prevState) => {
+			return { sideDrawerOpen: !prevState.sideDrawerOpen };
+		});
+	};
+
+	backdropClickHandler = () => {
+		this.setState({ sideDrawerOpen: false });
+	};
+
+	render() {
+		let backdrop;
+
+		if (this.state.sideDrawerOpen) {
+			backdrop = <Backdrop click={this.backdropClickHandler} />;
+		}
+		return (
+			<div className="login-page">
+				<Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+				<SideDrawer show={this.state.sideDrawerOpen} />
+				{backdrop}
+				<Container fluid className="login-container">
+					<header className="top-head"></header>
+					<div className="form">
+						<h1 className="login-title">Login</h1>
+						<p className="signup-link">
+							Don't have a Healthify account? <Link to="/signup">Sign Up</Link>
+						</p>
+						<Row className="login-section">
+							<Col xs className="login-email">
+								<Input
+									type="text"
+									label="Email"
+									name="userEmail"
+									fnc={this.getInputText}
+								></Input>
+								<Input
+									type="password"
+									label="Password"
+									name="userPassword"
+									fnc={this.getInputText}
+								></Input>
+								<Link to="/home">
+									<button
+										className="login-btn"
+										href="/home"
+										onClick={this.logIn}
+									>
+										Login
+									</button>
+								</Link>
+								<div className="sm-btns">
+									<a className="btn-floating btn-fb" href="#index">
+										<i className="fab fa-facebook-f"> </i>
+									</a>
+									<a className="btn-floating btn-gplus" href="#index">
+										<i className="fab fa-google-plus-g"> </i>
+									</a>
+								</div>
+							</Col>
+							<div className="login-social">
+								<div className="btn-container">
+									<button className="social-btn btn-facebook">
+										<img
+											src="https://static.parastorage.com/services/login-statics/1.742.0/images/facebook-logo.svg"
+											alt="img"
+										></img>
+										<span>Login with Facebook</span>
+									</button>
+								</div>
+								<div className="btn-container">
+									<button className="social-btn btn-google">
+										<img
+											alt="img"
+											src="https://static.parastorage.com/services/login-statics/1.742.0/images/google-logo.svg"
+										></img>
+										<span>Login with Google</span>
+									</button>
+								</div>
+							</div>
+						</Row>
+					</div>
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default login;
